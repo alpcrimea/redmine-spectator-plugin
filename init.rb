01af8f8edd302b4062e52c55583264067a4611d4
Alexey Pisarenko <alp@itcrimea.com>
2014-06-19 17:03:12 +0400

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :spectator do
+  name 'Spectator plugin'
+  author 'Alexey Pisarenko'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/alpcrimea/redmine-spectator-plugin'
+  author_url 'https://github.com/alpcrimea'
+end
